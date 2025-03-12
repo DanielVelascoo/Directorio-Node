925d5429aac86b0c86ce94d3c47e4a6e4f59de70
Daniel Velascoo <danielcamiloeskratos@gmail.com>
2025-03-12 17:26:07 -0500

    Aqui mejoras de oficina

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql2/promise");
+require("dotenv").config(); // Cargar variables de entorno desde .env
 
 // Configurar la conexión con MySQL
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "directorio",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
